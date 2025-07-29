8449713ea01f36514c3b5a339dae317b10e08833
userAryush <khatriaryush@gmail.com>
2025-07-29 21:40:55 +0545

    connected mysql

--- database.py
+++ database.py
@@ -3,8 +3,6 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 
 DATABASE_URL = "mysql+pymysql://root:Aryush%401234K@localhost/chatdb"
 
-
-
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
